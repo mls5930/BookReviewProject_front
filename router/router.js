60cfb307a5f8e03dcfd60c79476f4e36f6323dc1
mls <cjmadlife5930@gmail.com>
2025-02-05 20:23:37 +0900

    feat: 메인 html css 작업

--- router/router.js
+++ router/router.js
@@ -3,24 +3,22 @@ const router = express.Router()
 const path = require('path');
 const a = path.join(__dirname,`../views/main/main.html`)
 require('dotenv').config();
-
+const {bookData,bookData2} = require("../public/js/main")
 router.get('/' , (req,res) => {
     console.log(a);
-    
-    res.sendFile(a)
+    res.render(a ,{
+        bookData,
+        bookData2
+    })
 })
 const HOST = 'https://kauth.kakao.com'
 const REST_API_KEY = process.env.API_KEY
 
 router.get('/login/page', async (req, res) => {
-    const REDIRECT_URI = "http://localhost:3005/oauth/kakao";
+    const REDIRECT_URI = "http://localhost:3000/oauth/kakao";
     const redirectURL = `${HOST}/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
     res.redirect(redirectURL);
   });
 
 module.exports= router
 
-router.get("/oauth/kakao", async (req,res) => {
-    const { code } = req.qurey
-    
-})
