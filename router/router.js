03777e9f335e1d8b77432d1e2e7e1107a893c195
mls <cjmadlife5930@gmail.com>
2025-02-07 19:23:50 +0900

    feat: my review 페이지 작성

--- router/router.js
+++ router/router.js
@@ -14,11 +14,11 @@ router.get('/' , (req,res) => {
 const HOST = 'https://kauth.kakao.com'
 const REST_API_KEY = process.env.API_KEY
 
-// router.get('/login/page', async (req, res) => {
-//     const REDIRECT_URI = "http://localhost:3000/oauth/kakao";
-//     const redirectURL = `${HOST}/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
-//     res.redirect(redirectURL);
-//   });
+router.get('/login/page', async (req, res) => {
+    const REDIRECT_URI = "http://localhost:3000/oauth/kakao";
+    const redirectURL = `${HOST}/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+    res.redirect(redirectURL);
+  });
 router.get('/mypage', async (req, res) => {
     const mybookData = bookData.map( (book) => {
         return{
@@ -35,10 +35,29 @@ router.get('/mypage', async (req, res) => {
 })
 
 router.get('/myreview', async (req, res) => {
-    
+    // const bookData = await axios.get('http://localhost3000/bookList',{search:"비트코인"})
     res.render(mainHtml+`myreview.html`,{
+        bookData
+    })
+})
+
+router.get('/usermodify' , async (req, res) => {
+    res.render('main/userModify.html' ,{
         bookData
     })
 })
 module.exports= router
 
+
+/* axios.[HTTP메서드]([URL], [보낼데이터], [그외설정])
+    const response = await axios.post('/user/login', {
+        user_id: user_id.value,
+        user_pw: user_pw.value
+    }, {
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    })
+    console.log(response.data);
+    if(response.data.success) window.location.href = response.data.redirect
+*/
