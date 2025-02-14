ef3385b3ffefd11438e3e798df03bc94f96b9b45
sangah323 <choa323@naver.com>
2025-02-14 21:15:00 +0900

    fix: 책 상세, 감상문 상세, 감상문 작성, 수정 페이지 오류 수정

--- public/js/userReviewBtn.js
+++ public/js/userReviewBtn.js
@@ -1,30 +1,22 @@
-// Cookie에서 JWT_Token 가져옴
-function getNickname() {
-  const token = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("JWT_Token="))
-    ?.split("=")[1];
+const kakaoNickname = document.querySelector("#kakaoNickname");
+const updateBtn = document.querySelector(".update-btn");
 
-  if (!token) return null;
+console.log("script 들어옴");
 
-  //   JWT_Token payload 디코딩해서 nickname 가져옴
-  try {
-    const payload = JSON.parse(atob(token.split(".")[1]));
-    return payload.nickname;
-  } catch (error) {
-    console.error("토큰 오류:", error);
-    return null;
-  }
-}
+const getNickname = async () => {
+  // httpOnly는,, document로 읽어올 수 없음
+  // 내 nickname이랑 작성자 nickname이랑 비교
+  // controller.js에서 req.user.nickname으로 가져옴 => html에서 숨겨두고 가져오면 됨
+};
 
-document.addEventListener("DOMContentLoaded", function () {
-  const userNickname = getNickname(); // Cookie에서 가져온 JWT_Token nickname
-  const reviewNickname = "{{bookData.User.nickname}}"; // backend에서 받아온 nickname
-  //   const userNickname = "kim";
-  //   const reviewNickname = "kim";
+console.log(kakaoNickname.innerHTML);
 
-  //   nickname 일치할 경우에만 버튼 보여주기
-  if (userNickname && userNickname === reviewNickname) {
-    document.querySelector(".update-btn").style.display = "flex";
-  }
-});
+// document.addEventListener("DOMContentLoaded", async function () {
+//   const userNickname = await getNickname(); // Cookie에서 가져온 JWT_Token nickname
+//   const reviewNickname = kakaoNickname.innerHTML.trim(); // backend에서 받아온 nickname
+
+//   //   nickname 일치할 경우에만 버튼 보여주기
+//   if (userNickname && userNickname === reviewNickname) {
+//     updateBtn.style.display = "flex";
+//   }
+// });
